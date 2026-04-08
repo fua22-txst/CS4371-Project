0d077ae74f2334f1baa6c220bc0d8ea35fa00a76
fua22-txst <fua22@txstate.edu>
2026-04-08 14:27:07 -0500

    to-do changes comment for spoofing CNN

--- src/data_loader.py
+++ src/data_loader.py
@@ -94,11 +94,14 @@ def get_attack_category(file_name, class_config):
         
 def load_and_preprocess_data(data_dir, class_config):
     """Load, preprocess, and prepare data for training."""
+    # !!! TODO: PLEASE CHANGE THIS SECTION TO ONLY GRAB THE SPOOFING AND BENIGN FILES FOR THE TRAIN AND TEST DATA
     train_files = [f"{data_dir}/train/{f}" for f in os.listdir(f"{data_dir}/train") if f.endswith('.csv')]
     test_files = [f"{data_dir}/test/{f}" for f in os.listdir(f"{data_dir}/test") if f.endswith('.csv')]
 
     train_df = pd.concat([pd.read_csv(f).assign(file=f) for f in train_files], ignore_index=True)
     test_df = pd.concat([pd.read_csv(f).assign(file=f) for f in test_files], ignore_index=True)
+
+    # !!! 
 
     train_df['Attack_Type'] = train_df['file'].apply(lambda x: get_attack_category(x, class_config))
     test_df['Attack_Type'] = test_df['file'].apply(lambda x: get_attack_category(x, class_config))
