b97ba5c9d0e428798cb13ceaa0d90a8f2af476fa
Miguel-git-it <miguelsaucedo2345@gmail.com>
2026-04-21 22:11:55 -0500

    Update rnn_model.py

--- src/rnn_model.py
+++ src/rnn_model.py
@@ -2,13 +2,20 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import SimpleRNN, Dense
 
+# This function builds and compiles the RNN model
 def create_rnn_model(input_shape, num_classes):
     """Create and compile the RNN model."""
+    #first RNN layer, return sequence is true so that the next layer gets full output.
     model = Sequential()
+
+    #second layer
     # Recurrent neural network feature extractor
     model.add(SimpleRNN(128, return_sequences=True, input_shape=input_shape))
+
+    # Fully connected (dense) layer for learning patterns after RNN processing
     model.add(SimpleRNN(64))
 
+    #output layer
     # Classifier
     model.add(Dense(128, activation='relu'))
     model.add(Dense(num_classes, activation='softmax')) 
