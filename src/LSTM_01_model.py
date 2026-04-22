100b61c21f3b6596f69def41aa2c024243bc4720
fua22-txst <fua22@txstate.edu>
2026-04-22 14:28:27 -0500

    Commented LSTM_01_model.py

--- src/LSTM_01_model.py
+++ src/LSTM_01_model.py
@@ -5,12 +5,24 @@ from tensorflow.keras.layers import LSTM, Dense
 def create_lstm_model(input_shape, num_classes):
     """Create and compile the LSTM model."""
     model = Sequential()
+    # Add a 64-neuron Long Short Term Memory layer to start,
+    # replacing the original model's format 
+    # original: Conv1D -> Maxpooling1D -> Conv1D -> Maxpooling1D -> Flatten -> Dense
+    # LSTM: LSTM -> Dense
     model.add(LSTM(64, input_shape=input_shape))
+
+    # The Dense layer reconfigures the neuron's classifications as 
+    # something more human-readable for multiclassification tasks
     model.add(Dense(num_classes, activation='softmax'))
 
+    # Use the adam optimizer, multiclassification, and use accuracy as the primary metric
+    # This is the same as the original paper's model's final parameters.
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
+
+# This function is the same as original paper's model but with callbacks (checkpoints)
+# so that training can be paused/resumed
 def train_model(model, X_train, y_train_categorical, 
                 X_val, y_val_categorical, 
                 epochs=10, batch_size=32, checkpoint_callback=[], initial_epoch=0):
